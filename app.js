63bfa969ecd5feafd47a12cf35c4734304ddd6b5
Nick Doiron <ndoiron@mapmeld.com>
2012-12-31 12:29:50 +1200

    prepare for new locations

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var express = require('express')
 
 var HOUR_IN_MILLISECONDS = 3600000;
 
-var included_sources = [ "allegheny", "bloomington", "boston", "chicago", "kitsap", "lancaster", "oakland", "philadelphia", "pittsburgh", "sanfrancisco", "savannah", "seattle", "spokane" ];
+var included_sources = [ "allegheny", "austin", "baltimore", "bloomington", "boston", "chatham", "chicago", "kitsap", "lancaster", "nanaimo", "oakland", "philadelphia", "pittsburgh", "sagis", "sanfrancisco", "savannah", "seattle", "spokane", "westsacramento" ];
 
 var init = exports.init = function (config) {
   
