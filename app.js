aae6a46ec42a63fccefe8a8d8187436f6447b88d
Nick Doiron <ndoiron@mapmeld.com>
2013-01-03 10:46:54 +1200

    diagnose rest api prob

--- app.js
+++ app.js
@@ -479,6 +479,7 @@ var init = exports.init = function (config) {
         for(var pt=0;pt<poly.length;pt++){
           poly[pt] = [ poly[pt].split(",")[1] * 1.0, poly[pt].split(",")[0] * 1.0 ];
         }
+        return res.send(poly);
         timepoly.TimePoly.find({ src: req.params.src }).find({ ll: { "$within": { "$polygon": poly } } }).limit(10000).exec(function(err, timepolys){
           if(err){
             return res.send(err);
