6902e30f4dc13f693c1ac8c098880e3f13dee5bd
Nick Doiron <ndoiron@mapmeld.com>
2013-01-15 12:46:48 +1200

    add zoomByAbout, update README

--- scripts/pagesbuild.js
+++ scripts/pagesbuild.js
@@ -355,7 +355,14 @@ function describe(description){
   }
   return description;
 }
-
+function zoomByAbout(e) {
+  var x = .5*$('#map').width(),
+  y = .5*$('#map').height(),
+  mouse_point = e.containerPoint,
+  new_center_point = new L.Point((x + mouse_point.x) / 2, (y + mouse_point.y) / 2),
+  new_center_location = map.containerPointToLatLng(new_center_point);	           
+  map.setView(new_center_location, map.getZoom() + 1);
+}
 function showDataSource(){
   $("#creditmessage").modal("toggle");
 }
